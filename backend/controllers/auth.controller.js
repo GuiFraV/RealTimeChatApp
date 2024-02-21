373f7fe5c327960e05b46c87859d9ca86e041ddd
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-02-21 18:06:28 +0100

    Refactor login controller to handle authentication and generate token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,8 +57,30 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = () => {
-    console.log("loginUser");
+export const login = async(req, res) => {
+    try {
+        const {username,password} = req.body;
+        const user = await User.findOne({username})
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "Invalide username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user.id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+
+        
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error:"Internal Server Error"})
+    }
 }
 
 export const logout = () => {
