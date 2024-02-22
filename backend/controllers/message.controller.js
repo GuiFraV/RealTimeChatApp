ac2f59c347dcf68a4f74e69c4faef5f3f781f272
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-02-22 09:29:34 +0100

    Add conversation creation and message sending functionality

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,33 @@
+import Conversation from '../models/conversation.model.js'
+
 export const sendMessage = async (req,res) => {
    try {
 
     const {message} = req.body;
-    const {id} = req.params.id;
+    const {id:receiverId} = req.params.id;
     const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+        participantes: { $all: [senderId, receiverId]}
+    });
+
+    if(!conversation){
+        conversation = await Conversation.create({
+            participants : [senderId, receiverId],
+        })
+    }
+
+    const newMessage = new Message({
+        senderId,
+        receiverId,
+        message,
+    })
+
+    if(newMessage){
+        conversation.messages.push(newMessage._id);
+    }
+
+    res.status(201).json(newMessage);
     
    } catch (error) {
         console.log("Error in sendMessage controller : ", error.message)
