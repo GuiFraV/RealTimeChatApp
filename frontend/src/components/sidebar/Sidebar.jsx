132c2275de3af565c8fbaf6280cd4cd7d73fa7e7
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-02-26 15:56:57 +0100

    Add SignUp component and update Home component

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+  return (
+    <div>
+        <SearchInput />
+        <div className="divider px-3"></div>
+        {/* <Conversation />
+        <LogoutButton /> */}
+    </div>
+  )
+}
+
+export default Sidebar
