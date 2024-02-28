3a45f5be62359702a2bfb993a64f321ec244203a
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-02-28 17:57:01 +0100

    Add react-router-dom dependency and implement routing in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Home from './pages/homepage/Home'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/SignUp'
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   )
 }
