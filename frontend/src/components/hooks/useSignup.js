686235fcd5a2536132ae197a83a7ea83b9e6af87
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-05 10:51:36 +0100

    Refactor useSignup hook and add proxy configuration

--- frontend/src/components/hooks/useSignup.js
+++ frontend/src/components/hooks/useSignup.js
@@ -1,39 +1,39 @@
-import { useState } from "react"
+import { useState } from "react";
+import toast from "react-hot-toast";
 
-const useSignup = () {
+const useSignup = () => {
+	const [loading, setLoading] = useState(false);
+	// const { setAuthUser } = useAuthContext();
 
-    const [loading, setLoading] = useState(false)
+	const signup = async ({ fullName, username, password, confirmPassword, gender }) => {
+		const success = handleInputErrors({ fullName, username, password, confirmPassword, gender });
+		if (!success) return;
 
-    const signup = async({fullName, username, password, confirmPassword, gender}) => {
-        const success = handleInputErrors({fullName, username, password, confirmPassword, gender})
-        if(!success) return;
+		setLoading(true);
+		try {
+			const res = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ fullName, username, password, confirmPassword, gender }),
+			});
 
-        setLoading(true);
+			const data = await res.json();
+			if (data.error) {
+				throw new Error(data.error);
+			}
+			// localStorage.setItem("chat-user", JSON.stringify(data));
+			// setAuthUser(data);
+		} catch (error) {
+			toast.error(error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
 
-        try {
+	return { loading, signup };
+};
 
-            const res = await fetch("http://localhost:5000/api/auth/signup",{
-                method: "POST",
-                headers : { "Content-Type" : "application/json"},
-                body: JSON.stringify({fullName, username, password, confirmPassword, gender})
-            })
-
-            const data = await res.json();
-            console.log(data);
-
-        }catch(error){
-            toast.error(error.message)
-        }finally{
-            setLoading(false);
-        }
-    
-    }
-
-    return { loading, signup };
-
-}
-
-export default useSignup
+export default useSignup;
 
 function handleInputErrors({fullName, username, password, confirmPassword, gender}){
     if(!fullName || !username || !password || !confirmPassword || !gender){
