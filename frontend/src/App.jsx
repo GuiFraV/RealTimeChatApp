cf558c6ab1cc1590f449da203dfe49f969a8cbd0
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-14 09:52:13 +0100

    Add authentication to signup route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,20 @@ import './App.css'
 import Home from './pages/homepage/Home'
 import Login from './pages/login/Login'
 import Signup from './pages/signup/SignUp'
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
+import { useAuthContext } from './context/AuthContext';
 
 function App() {
+
+  const {authUser} = useAuthContext();
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
       </Routes>
       <Toaster />
     </div>
