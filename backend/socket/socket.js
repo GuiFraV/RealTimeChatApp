8cbe6f7c916fee93156fff87104ef1666d17a735
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-03-15 09:43:25 +0100

    Add userSocketMap and online user functionality

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -10,14 +10,23 @@ const io = new Server(server,{
         origin:["http://localhost:3000"],
         methods:["GET", "POST"]
     }
-})
+});
+
+const userSocketMap = {};
 
 
 io.on('connection',(socket) => {
     console.log("a user connected", socket.id);
 
+    const userId = socket.handshake.query.userId;
+    if(userId != "undefined") userSocketMap[userId] = socket.id;
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
     socket.on("disconnect", () => {
         console.log("user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 })
 
